d49b20fc2bf9c6ec369df3bab8fc775527f7b49d
IVIGS <ivigamesteam@gmail.com>
2023-07-14 15:13:28 -0500

    Hay que eliminar un archivo

---
+++ Personajes del señor de los anillos.py
@@ -0,0 +1,18 @@
+"""Escriba un programa donde cree una lista con los siguientes personajes del Señor de los anillos
+ 	Nombre: Aragorn
+ 	Clase: Guerrero
+ 	Raza: Dunadan del norte 	
+ 	Nombre: Legolas
+ 	Clase: Arquero
+ 	Raza: Elfo Sindar
+ 	Nombre: Gandalf
+ 	Clase: Mago
+ 	Raza: Istar """
+
+
+personaje1 = {"Nombre":"Aragorn", "Clase":"Guerrero", "Raza":"Dunadan del norte"}
+personaje2 = {"Nombre":"Legolas", "Clase":"Arquero", "Raza":"Elfo Sindar"}
+personaje3 = {"Nombre":"Gandalf", "Clase":"Mago", "Raza":"Istar"}
+personajes = [personaje1, personaje2, personaje3]
+
+print(personajes,end="---")
